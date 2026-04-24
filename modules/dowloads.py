5086adaa9525c6e022a9527cb7098620274a8837
Nopparatz1238 <uiavgcgccggv@gmail.com>
2026-04-24 17:50:23 +0700

    update modules dowloads

---
+++ modules/dowloads.py
@@ -0,0 +1,27 @@
+import requests
+import os
+
+class Dowloads:
+    @staticmethod
+    def start_download(url, save_path, progress_callback):
+        try:
+            os.makedirs(os.path.dirname(save_path), exist_ok=True)
+            
+            response = requests.get(url, stream=True)
+            total_size = int(response.headers.get('content-length', 0))
+            
+            with open(save_path, 'wb') as f:
+                if total_size == 0:
+                    f.write(response.content)
+                else:
+                    downloaded = 0
+                    for data in response.iter_content(chunk_size=4096):
+                        downloaded += len(data)
+                        f.write(data)
+                    
+                        progress = downloaded / total_size
+                        progress_callback(progress)
+            return True
+        except Exception as e:
+            # print(e)
+            return False
