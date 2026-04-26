a8e8d62abb64b23904064a4aa79c126034a68464
Nopparatz1238 <uiavgcgccggv@gmail.com>
2026-04-26 17:15:44 +0700

    update version 1.0"

--- main.py
+++ main.py
@@ -4,51 +4,31 @@ from imgui_bundle import imgui as ImGui, immapp, hello_imgui, icons_fontawesome_
 from modules import config
 from modules.downloads import Dowloads
 from modules.gui import Gui
-from modules.config import state, Config
+from modules.config import state, Config, resource_path
 from modules.ventoy import Ventoy
 
-
-
-
+font_with_icons = None
 font_with_icons = None
 
-def load_fonts():
-    global font_with_icons
-    font_config = ImGui.ImFontConfig()
-    font_config.merge_mode = True
-    font_config.pixel_snap_h = True
-
-    io = ImGui.get_io()
-    font_with_icons = io.fonts.add_font_default() 
-
-    font_size = 16.0
-    io.fonts.add_font_from_file_ttf(
-        hello_imgui.asset_file_path("fonts/fontawesome-webfont.ttf"),
-        font_size,
-        font_config,
-        icons_fontawesome_6.get_glyph_ranges_font_awesome_6()
-    )
-
-with open('os.json', 'r', encoding='utf-8') as file:
+with open(resource_path('os.json'), 'r', encoding='utf-8') as file:
     os_data = json.load(file)
 
 windows_list = os_data['window']
 linux_list = os_data['linux']
 
 def gui():
-    if font_with_icons is not None:
-        ImGui.push_font(font_with_icons)
 
     style = ImGui.get_style()
     ImGui.style_colors_dark()
     style.window_rounding = 15.0
     style.frame_rounding = 5.0
 
+    ImGui.set_next_window_pos(ImGui.ImVec2(0, 0), ImGui.Cond_.always)
     ImGui.set_next_window_size(ImGui.ImVec2(900, 600), ImGui.Cond_.always)
     ImGui.push_style_var(ImGui.StyleVar_.window_padding, ImGui.ImVec2(0, 0))
     ImGui.push_style_color(ImGui.Col_.window_bg, ImGui.ImColor(*Config.color_background).value)
-    ImGui.push_style_color(ImGui.Col_.border, ImGui.ImColor(0, 0, 0, 255).value)
-    ImGui.begin("ISO Downloader", None, ImGui.WindowFlags_.no_title_bar | ImGui.WindowFlags_.no_resize) 
+    ImGui.push_style_color(ImGui.Col_.border, ImGui.ImColor(0, 0, 0, 0).value) # Remove border line
+    ImGui.begin("ISO Downloader", None, ImGui.WindowFlags_.no_title_bar | ImGui.WindowFlags_.no_resize | ImGui.WindowFlags_.no_move | ImGui.WindowFlags_.no_collapse | ImGui.WindowFlags_.no_bring_to_front_on_focus) 
 
     Gui.render_screen(windows_list, linux_list)
     Gui.render_particles()
@@ -59,11 +39,16 @@ def gui():
     ImGui.pop_style_color(2)
     ImGui.pop_style_var()
 
-    if font_with_icons is not None:
-        ImGui.pop_font()
-
 def render():
-    immapp.run(gui_function=gui, window_title="OS Downloader", window_size=(500, 400))
+    runner_params = immapp.RunnerParams()
+    runner_params.app_window_params.window_title = "OS Downloader"
+    runner_params.app_window_params.window_geometry.size = (900, 600)
+    runner_params.app_window_params.resizable = False
+    runner_params.imgui_window_params.default_imgui_window_type = hello_imgui.DefaultImGuiWindowType.no_default_window
+    runner_params.callbacks.show_gui = gui
+    runner_params.callbacks.default_icon_font = hello_imgui.DefaultIconFont.font_awesome6
+    
+    immapp.run(runner_params=runner_params)
 
 if __name__ == "__main__":
     render()
--- modules/ventoy.py
+++ modules/ventoy.py
@@ -1,3 +1,5 @@
+# https://github.com/ventoy/Ventoy
+
 import psutil
 import subprocess
 import os
